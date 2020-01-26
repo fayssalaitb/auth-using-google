bf9cfba364d81b4e868e7045650fc67f2b9e5d1f
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2020-01-26 14:03:14 +0100

    installed react and next figure out how to start both servers at the same time

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieSession = require('cookie-session');
-const passport = require('passport');
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 
-require('./models/User')
+require("./models/User");
 require("./services/passport");
 //require("./models/User");
 const authRoutes = require("./routes/authRoutes");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI, { useUnifiedTopology: true, useNewUrlParser: true, ssl:true  }).catch(error=>console.log(JSON.stringify(error)))
-
+mongoose
+  .connect(keys.mongoURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    ssl: true
+  })
+  .catch(error => console.log(JSON.stringify(error)));
 
 const app = express();
 app.use(
   cookieSession({
-    maxAge: 30*24*60*1000,
+    maxAge: 30 * 24 * 60 * 1000,
     keys: [keys.cookieKey]
   })
-)
+);
 
-app.use(passport.initialize())
-app.use(passport.session())
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-
-
-
-
 
 // const MongoClient = require('mongodb').MongoClient;
 // const uri = "mongodb+srv://admin-fayssal:MAC13dell45@cluster0-0cwlt.mongodb.net/test?retryWrites=true&w=majority";
@@ -43,4 +42,3 @@ app.listen(PORT);
 //   // perform actions on the collection object
 //   client.close();
 // });
-
