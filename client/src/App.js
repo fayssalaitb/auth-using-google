36ec5adcd620909477e2b488f8a3989f27c93148
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2020-01-26 14:57:19 +0100

    installed http-proxy-middleware on the package.jsom client to help redirect the link from --:5000 to --:3000

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="/auth/google">Sign In With Google</a>
         <a
           className="App-link"
           href="https://reactjs.org"
